fbfaca9a51205227cede6f40b80f039a4b22cbb0
sheikxm <sheikspy0531@gmail.com>
2024-10-19 19:50:17 +0530

    partially done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import Heading from './components/Heading';
 
 const App: React.FC = () => {
     return (
-        <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
-            <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+        <div className="h-100  flex items-center justify-center bg-teal-lightest font-sans">
+            <div className="bg-white rounded shadow p-6 m-4 w-full ">
+                <Heading/>
                 <TodoList />
             </div>
         </div>
